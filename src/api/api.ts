ae1dbd3b50ade23d67d07d551986cd93f8588923
maksim.petrov00 <maksim.petrov@triona.fi>
2022-12-20 16:04:15 +0200

    Scraping moved to api action

---
+++ src/api/api.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { Context, Telegraf } from "telegraf";
+import { Update } from "telegraf/typings/core/types/typegram";
+import { ScrapingService } from "../service/scrapingService";
+
+export class RestAPI {
+  constructor(bot: Telegraf<Context<Update>>) {
+    const app = express();
+    app.use(cors());
+    app.get("/run", (req, res) => {
+        console.log('Request received')
+        ScrapingService.scrapeAllTrackers(bot)
+    });
+    app.listen(8080, () => console.log(`Server started on port 8080`));
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import { CHAT_ID } from './constants/chatId';
 import { API_KEY } from './constants/apiKey';
 import { Bot } from "./bot/bot"
 import { DatabaseRequestor } from './service/databaseRequestor';
+import { RestAPI } from './api/api';
 
 class Main {
     constructor() {
         const db = new DatabaseRequestor()
-        const bot = new Bot(API_KEY, CHAT_ID)
-        const interval = setInterval(() => {
-            ScrapingService.scrapeAllTrackers(bot.bot)
-        }, 10000)
+        const { bot } = new Bot(API_KEY, CHAT_ID)
+        const api = new RestAPI(bot)
         
         console.log('Service is up and running')
     }
